6e63cbbaae5eb605f703549d3cdc940862e60bba
Kimchooman <43964171+Kimchooman@users.noreply.github.com>
2019-06-11 22:25:13 -0400

    Update cam.py

--- cam.py
+++ cam.py
@@ -17,37 +17,46 @@ class camera:
 		self.hog = cv2.HOGDescriptor()
 		self.hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
+	def find_W_H(self,x,y,x1,y1):
+
+		w = x-x1
+		h = y-y1
+		return w, h
+	
 	def max_body_seen(self, pick):
+	
 		max = 0
+		max_w, max_h = self.find_W_H(pick[max][0], pick[max][1], pick[max][2], pick[max][3])
 
 		for i in range(len(pick)):
-			if pick[i][2] * pick[i][3] > pick[max][2] * pick[max][3]:
+			w, h = self.find_W_H(pick[i][0], pick[i][1], pick[i][2], pick[i][3])
+
+			if w * h > max_w * max_h:
 				max = i
 		
 		return max
 
 	def snap(self):
 		r, image = self.cam.read()
+		print("Snapped")
 
 		if r:
-
 			image = imutils.resize(image, width=image.shape[1])
 			rects, weights = self.hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
 
 			rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
 			pick = non_max_suppression(rects,probs=None, overlapThresh=0.65)
 			
-			index_of_max = self.max_body_seen(pick) 
-			
-			if pick is []:
+			if pick == []:
 				return None
+
 			else:
-				print(pick)
-				return pick[index_of_max][0] + pick[index_of_max][2], pick[index_of_max][1] + pick[index_of_max][3]
-			
+				index_of_max = self.max_body_seen(pick) 
+				return pick[index_of_max][0]
+
 		else:
 			return -1
 
 	def end_session(self):
 		print(f"Started at --> {self.start_time} \n Ended at --> {datetime.now()}")
-		cam.release()
+		self.cam.release()
