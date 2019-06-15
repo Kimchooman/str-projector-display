656a961e308b219833fd82c7072a20a6ecb65397
Kimchooman <43964171+Kimchooman@users.noreply.github.com>
2019-06-15 13:56:41 -0400

    Create sub_modules.py

---
+++ sub_modules.py
@@ -0,0 +1,44 @@
+import numpy as np
+import time
+import math
+
+class alarm:
+	def __init__(self,start, time):
+		self.start = start
+		self.end = start + time
+
+	def check(self):
+		if int(time.time()) >= self.end: return True
+		
+		else: return False
+
+class point:
+	def __init__(self,x,y):
+		self.x = x
+		self.y = y
+
+#!~!~~!~!~!~!~! note: this is code from the internet (lines 20-26)
+def split(array, nrows, ncols): 
+    """Split a matrix into sub-matrices."""
+
+    r, h = array.shape
+    return (array.reshape(h//nrows, nrows, -1, ncols)
+                 .swapaxes(1, 2)
+                 .reshape(-1, nrows, ncols))
+
+def distance(x, y, x1, y1):
+	return math.sqrt((x - x1)**2 + (y - y1)**2)
+
+def lerp(p, p1, factor):
+	return ((p1 - p) * factor)
+
+def reverse_lerp(p, p1, factor):
+	return -(factor // (p1-p))
+
+def max(num, num1):
+
+    if num > num1:
+        return 0
+    
+    else:
+        return 1
